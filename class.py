677318e1de650784ea8803a023ff7a0973f69e73
kaynat-Z <kaynat.zfr000@gmail.com>
2025-09-18 21:04:21 +0500

    operators

--- class.py
+++ class.py
@@ -9,3 +9,18 @@ z = "welcome"
 print(z)
 school = "student"
 print(school)
+firstName = input("Enter your first name: ") 
+print("Your first name is: " , firstName)
+
+num1 = int(input("first number:"))
+num2 = int(input("second number:"))
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+print("The sum is:", sum)
+
+subtr = num1 -num2
+print("The subtraction is:", subtr)

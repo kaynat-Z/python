0e2639d09bab814086f65167b7d391c3c96e11e0
kaynat-Z <kaynat.zfr000@gmail.com>
2025-09-20 21:14:31 +0500

    operators1

--- class.py
+++ class.py
@@ -24,3 +24,31 @@ print("The sum is:", sum)
 
 subtr = num1 -num2
 print("The subtraction is:", subtr)
+
+
+# operators in python
+#
+#
+# Arithmetic operators
+#
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+print("The sum is:", sum)
+
+subtr = num1 - num2
+print("the subtraction is:", subtr)
+
+mult = num1 * num2
+print("the mutiplication is:", mult)
+
+div = num1 / num2
+print("the division is:", div)
+
+mod = num1 % num2
+print("the modulus is:", mod)
+
+floor = num1 // num2
+print("the floor division is:", floor)
+

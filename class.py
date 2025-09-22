8c9b502585a06c32fc29d8931f2ef6bece131c01
kaynat-Z <kaynat.zfr000@gmail.com>
2025-09-22 19:48:23 +0500

    assignment0

--- class.py
+++ class.py
@@ -3,10 +3,22 @@
 # 
 #
 #
+#Q1
 
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
 print("your name is:", name, "and your age is:", age)
+#Q2
+color = input ( "Enter your favorite color: " )
+print("Your favorite color", color)
 
-
+#Q3
+#Ask for two words from the user and join them with a space
+word1 = input("Enter first word: ")
+word2 = input("Enter second word: ")
+combined_words = word1 + " " + word2
+print("combined words:", combined_words)
+ #Q4
+ #Write a program that asks for the user’s birth year and current year. Then print their age.
+ birth_year = int(input)

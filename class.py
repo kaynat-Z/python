e39cbe20841c3b891908a446c0307da4f1798437
kaynat-Z <kaynat.zfr000@gmail.com>
2025-09-22 19:23:25 +0500

    assignment

--- class.py
+++ class.py
@@ -1,54 +1,12 @@
-print("Hello, World!")
-print('kaynat')
-x = "kaynat"
-print(x)
-# class variables
-y = 10
-print(y)
-z = "welcome"
-print(z)
-school = "student"
-print(school)
-firstName = input("Enter your first name: ") 
-print("Your first name is: " , firstName)
 
-num1 = int(input("first number:"))
-num2 = int(input("second number:"))
-
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-sum = num1 + num2
-print("The sum is:", sum)
-
-subtr = num1 -num2
-print("The subtraction is:", subtr)
-
-
-# operators in python
+#Assignment 
+# 
 #
 #
-# Arithmetic operators
-#
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
 
-sum = num1 + num2
-print("The sum is:", sum)
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
 
-subtr = num1 - num2
-print("the subtraction is:", subtr)
+print("your name is:", name, "and your age is:", age)
 
-mult = num1 * num2
-print("the mutiplication is:", mult)
-
-div = num1 / num2
-print("the division is:", div)
-
-mod = num1 % num2
-print("the modulus is:", mod)
-
-floor = num1 // num2
-print("the floor division is:", floor)
 

e8d46232b2106c064ecdd497953bb3bd9c3ee549
kaynat-Z <kaynat.zfr000@gmail.com>
2025-10-08 08:59:01 +0500

    match case 2

--- class.py
+++ class.py
@@ -1,29 +1,55 @@
 
-#Assignment 
-# 
-#
-#
-#Q1
+# #Assignment 
+# # 
+# #
+# #
+# #Q1
 
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
 
-print("your name is:", name, "and your age is:", age)
-#Q2
-color = input ( "Enter your favorite color: " )
-print("Your favorite color", color)
+# print("your name is:", name, "and your age is:", age)
+# #Q2
+# color = input ( "Enter your favorite color: " )
+# print("Your favorite color", color)
 
-#Q3
-#Ask for two words from the user and join them with a space
-word1 = input("Enter first word: ")
-word2 = input("Enter second word: ")
-combined_words = word1 + " " + word2
-print("combined words:", combined_words)
- #Q4
- #Write user’s birth year and current year. Then print their age.
+# #Q3
+# #Ask for two words from the user and join them with a space
+# word1 = input("Enter first word: ")
+# word2 = input("Enter second word: ")
+# combined_words = word1 + " " + word2
+# print("combined words:", combined_words)
+#  #Q4
+#  #Write user’s birth year and current year. Then print their age.
 
-birth_year = int(input ("Enter your birth year: "))
-current_year = int(input("Enter current year: "))
-age = current_year - birth_year
+# birth_year = int(input ("Enter your birth year: "))
+# current_year = int(input("Enter current year: "))
+# age = current_year - birth_year
 
-print("Your age is :", age)
+# print("Your age is :", age)
+
+# #Q5
+# #the price of a product and a discount percentage
+
+# price_of_product = float (input("Enter the price of the product: "))
+# discount_percentage = float (input("Enter the discount percentage: "))
+# discount_percentage = price_of_product * (discount_percentage /100 )
+
+# final_price = price_of_product - discount_amount
+
+# print("The final price after discount is:", final_price)
+
+
+
+# # user's percentage and print their grade based on the percentage.
+# #
+# percentage = float (input("Enter your percentage: "))
+# if (percentage >= 90):
+#     grade = "A"
+# elif (percentage >= 80):
+#     grade = "B"
+# else:
+#     grade = "C"
+
+for i in range(1,21):
+    print(i)
